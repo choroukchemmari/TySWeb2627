7936ff719fbd15ba6f91b47430cce372e54a98ba
chorouk chemmari <chorouk.chemmari@alu.uclm.es>
2026-09-20 16:55:45 +0200

    Corregir URL de bealquileres en desarrollo

--- feayto/src/environments/environment.ts
+++ feayto/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   
-  bealquileresUrl: 'http://localhost:8081',
+  bealquileresUrl: 'http://localhost:8081/api',
   simuladorUrl: 'http://localhost:8082'
 };
